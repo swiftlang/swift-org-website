374e48020057a968b42ea0cbec1e09aaa02a2f6e
Natik Gadzhi <natik@respawn.io>
2023-09-11 21:52:17 -0700

    Extract links check into a separate docker-compose job

--- scripts/check_links.sh
+++ scripts/check_links.sh
@@ -3,7 +3,6 @@ here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$here/.."
 
 bundle check
-bundle exec jekyll doctor --source /srv/jekyll --destination /output
 bundle exec jekyll build --source /srv/jekyll --destination /output
 
 # This line runs broken link checks: https://github.com/gjtorikian/html-proofer
