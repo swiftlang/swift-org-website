2da378166e76b7a53effb20d099051b055a3bdef
James Dempsey <dempsey@tapas-software.net>
2024-03-19 03:06:34 -0700

    Add ability for dashboard script to work with current and proposed schemas

--- assets/javascripts/swift-evolution.js
+++ assets/javascripts/swift-evolution.js
@@ -158,12 +158,15 @@ function init() {
   var req = new window.XMLHttpRequest()
 
   req.addEventListener('load', function() {
-    proposals = JSON.parse(req.responseText)
-
-    // Don't display malformed proposals
-    proposals = proposals.filter(function (proposal) {
-      return !proposal.errors
-    })
+    let evolutionMetadata = JSON.parse(req.responseText, flattenStatus)
+    
+    // Temporary conditional to allow script to work with old and new schemas
+    if (Array.isArray(evolutionMetadata)) { // current schema
+      proposals = evolutionMetadata
+    } else { // new schema
+      proposals = evolutionMetadata.proposals
+      languageVersions = evolutionMetadata.implementationVersions
+    }
     
     // Add upcomingFeatureFlag to proposal if present in mapping.
     // Temporary until upcomingFeatureFlag property is returned in proposals.json. 
@@ -199,6 +202,18 @@ function init() {
   document.querySelector('#proposals-count-number').innerHTML = 'Loading…'
   req.open('get', EVOLUTION_METADATA_URL)
   req.send()
+}
+
+/** 
+ * Reviver function passed to JSON.parse() to convert new status field structure to old structure.
+ */
+function flattenStatus(key, value) {
+  if (key == "status" && value !== "" && !value.state) {
+    let [subkey, subvalue] = Object.entries(value)[0]
+    subvalue.state = "." + subkey
+    return subvalue
+  }
+  return value
 }
 
 /**
